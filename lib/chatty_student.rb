f0a1fab91355600c7fcf552280dad9ace818018a
Carla Holcomb <carla.programmer@gmail.com>
2019-11-13 03:56:29 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,7 +2,8 @@ class ChattyStudent < Student
 
 def hello
 super
-"Hello how are you"
+"How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy
+ny spoilers? Okay well let me just tell you who died..."
 end
 
 
